91325714f64ad9542ba373c7cd87d340e3ca3389
Stuart Longland <me@vk4msl.com>
2024-07-10 08:47:35 +1000

    pic: Replace glib types and clean up
    - Sort the struct members from biggest alignment to smallest to better
      respect alignment constraints on some architectures (e.g. ARM)
    - Define a constant for the length of `timestr`

--- pic.h
+++ pic.h
@@ -1,16 +1,17 @@
 #ifndef _PIC_H_
 #define _PIC_H_
 
-#include <glib.h>
-#include <glib/gtypes.h>
+#include <stdint.h>
+
+#define PIC_TIMESTR_SZ (40)
 
 typedef struct _PicMeta PicMeta;
 struct _PicMeta {
-  gshort HedrShift;
-  guchar Mode;
   double Rate;
-  int    Skip;
-  char   timestr[40];
+  int32_t Skip;
+  int16_t HedrShift;
+  uint8_t Mode;
+  char   timestr[PIC_TIMESTR_SZ];
 };
 extern PicMeta CurrentPic;
 
